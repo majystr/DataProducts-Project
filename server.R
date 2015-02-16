bcf86fa14d452465cde6b0c7d119cd9e9b52413f
majystr <ken_graves@yahoo.com>
2015-02-16 17:28:41 -0500

    Primary Build

--- server.R
+++ server.R
@@ -7,8 +7,15 @@ data(iris)
 shinyServer(
     function(input, output) {
         output$plot <- renderPlot({
+            set.seed(10)
             results <- kmeans(iris[,-5],input$k_num)
-            plot(iris[c("Petal.Length", "Petal.Width")], col = results$cluster)
+            plot(iris[c("Petal.Length", "Petal.Width")], col = results$cluster,
+                 main = "Cluster Groupings by IRIS data Petal Width & Length")
+        })
+        output$table <- renderTable({
+            set.seed(10)
+            results <- kmeans(iris[,-5],input$k_num)
+            table(iris$Species,results$cluster)
         })
     }
 )
--- ui.R
+++ ui.R
@@ -6,11 +6,21 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Kmeans Clustering of IRIS data"),
     sidebarPanel(
-        radioButtons('k_num', 'Choose Number of Clusters:',
-                     list('2', '3', '4', '5'))
+        p("Kmeans is an unsupervised machine learning technique that attempts to identify
+           groupings within a dataset.  The number of initial group (k) influences the accuracy
+          of the final decision boundaries"),
+        p("This application demonstrates 'k' number group selection on the IRIS dataset included with R.
+           To see the effect on species predictions, please select number of cluster groupings to use with the Kmeans algorithm."),
+        radioButtons('k_num', 'Choose Number of Grouping Clusters:',
+                     list('2', '3', '4', '5', '6')),
+        p("The famous IRIS dataset contains data on 50 flowers of each of the 3 species of iris. 
+          Kmeans uses sepal and petal width and length to group each observation into a group.   The best fit should be a group of 3.")
     ),
     mainPanel(
-        plotOutput('plot')
+        plotOutput('plot'),
+        h4("Confusion Matrix"),
+        p("Columns represent the cluster counts by Kmeans groupings, while the rows show the actual species membership."),
+        tableOutput('table')
     )
 ))
 
